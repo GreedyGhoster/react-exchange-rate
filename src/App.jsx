29e2f8954a9cd7eadf357268c6227bbf75500877
IuanPR <103894461+IuanPR@users.noreply.github.com>
2023-01-06 00:48:24 +0300

    Complete project

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,14 @@ import { useEffect, useState } from "react";
 import { Block } from "./components/Block";
 
 export default function App() {
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [fromPrice, setFromPrice] = useState(0);
+  const [toPrice, setToPrice] = useState(0);
+  const [toCurrency, setToCurrency] = useState("EUR");
   const [rates, setRates] = useState({});
+
   useEffect(() => {
-    fetch("https://api.exchangerate.host/latest?base")
+    fetch("https://www.cbr-xml-daily.ru/latest.js")
       .then((res) => res.json())
       .then((json) => {
         setRates(json.rates);
@@ -16,15 +21,38 @@ export default function App() {
         alert("Ошибка при получении курса валют");
       });
   }, []);
-  console.log(rates.USD);
+
+  const onChangeFromPrice = (value) => {
+    const price = value / rates[fromCurrency];
+    const result = price * rates[toCurrency];
+    setToPrice(result.toFixed(4));
+    setFromPrice(value);
+  };
+  const onChangeToPrice = (value) => {
+    const result = (rates[fromCurrency] / rates[toCurrency]) * value;
+    setFromPrice(result.toFixed(4));
+    setToPrice(value);
+  };
+  useEffect(() => {
+    onChangeFromPrice(fromPrice);
+  }, [fromCurrency]);
+  useEffect(() => {
+    onChangeToPrice(toPrice);
+  }, [toCurrency]);
   return (
     <div className="App">
       <Block
-        value={0}
-        currency="RUB"
-        onChangeCurrency={(cur) => console.log(cur)}
+        value={fromPrice}
+        currency={fromCurrency}
+        onChangeCurrency={setFromCurrency}
+        onChangeValue={onChangeFromPrice}
       />
-      <Block value={0} currency="USD" />
+      <Block
+        value={toPrice}
+        currency={toCurrency}
+        onChangeCurrency={setToCurrency}
+        onChangeValue={onChangeToPrice}
+      />
     </div>
   );
 }
